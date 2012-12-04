1d0cf68874454f5ef973ae64f0d210b66c8d25a1
Yury Samokhvalov <y.samokhvalov@gmail.com>
2012-12-04 21:02:26 +1100

    5.1 completed

---
+++ week5/homework5.1.js
@@ -0,0 +1,19 @@
+use blog
+db.posts.aggregate([
+    {
+        "$unwind": "$comments"
+    },
+    {
+       "$group": 
+       {
+            "_id": "$comments.author",
+            "count": {$sum:1}           
+       }
+    },
+    {
+        "$sort": {'count': -1}
+    },
+    {
+        "$limit": 1
+    },
+])
